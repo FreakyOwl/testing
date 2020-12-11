5a5198bbcaa0c9416c4f70fb6c4f961f1a278956
FreakyOwl <69248671+FreakyOwl@users.noreply.github.com>
2020-12-11 14:47:37 +0300

    Update scriptfortests.py

--- scriptfortests.py
+++ scriptfortests.py
@@ -1,23 +1,28 @@
+"""
+this is a docstring
+"""
 import urllib.request
 import json
 listofurls = open('123.txt', 'r')
 listsoffriends = open('friendsurls.txt', 'w')
-domain = ''
+Domain = ''
 for line in listofurls:                 # получаем информацию о пользователях за один запрос
-    domain = domain+line[15:len(line)-1]+','  # получаем короткое название из url
-with urllib.request.urlopen("https://api.vk.com/method/users.get?user_ids="+domain[:len(domain)-1]
-     + "&v=5.52&access_token=b1b6da65b1b6da65b1b6da6547b1c57304bb1b6b1b6da65eefec3e8bb3149f4bcc040dc") as userurl:
+    Domain = Domain+line[15:len(line)-1]+','  # получаем короткое название из url
+with urllib.request.urlopen("https://api.vk.com/method/users.get?user_ids="+Domain[:len(Domain)-1]+
+     "&v=5.52&access_token=b1b6da65b1b6da65b1b6da6547b1c57304bb1b6b1b6da65eefec3e8bb3149f4bcc040dc") 
+     as userurl:
     userinfo = json.load(userurl)
 for item in userinfo.get('response'):
-    personid = str(item.get('id'))        # получаем ID пользователя
-    print(personid)
-    with urllib.request.urlopen("https://api.vk.com/method/friends.get?user_id="+personid +
-         "&v=5.52&access_token=b1b6da65b1b6da65b1b6da6547b1c57304bb1b6b1b6da65eefec3e8bb3149f4bcc040dc") as friendsurl:
+    PersonID = str(item.get('id'))        # получаем ID пользователя
+    print(PersonID)
+    with urllib.request.urlopen("https://api.vk.com/method/friends.get?user_id="+PersonID +
+         "&v=5.52&access_token=b1b6da65b1b6da65b1b6da6547b1c57304bb1b6b1b6da65eefec3e8bb3149f4bcc040dc")
+         as friendsurl:
         friendsinfo = json.load(friendsurl)     # получаем список друзей
         print(friendsinfo)
     for friendid in friendsinfo.get('response').get('items'):   # формируем список url-ов друзей
-        friendurl = "https://vk.com/id"+str(friendid)
-        listsoffriends.write(friendurl + '\n')
+        FriendURL = "https://vk.com/id"+str(friendid)
+        listsoffriends.write(FriendURL + '\n')
     listsoffriends.write('\n')
 listofurls.close()
 listsoffriends.close()
