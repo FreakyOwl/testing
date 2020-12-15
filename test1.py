d010fc7a621c056e1bb8123afa4d47c79a6e46a6
FreakyOwl <69248671+FreakyOwl@users.noreply.github.com>
2020-12-15 16:13:29 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,3 +5,9 @@ import scriptfortests
 def test_exception():
     with pytest.raises(FileNotFoundError):
         scriptfortests.getdata('1234.txt')
+        
+        
+def test_fileformat():
+    with open('123.txt', 'r') as listofurls:
+        for line in listofurls:
+            assert line[-1] == '\n'
