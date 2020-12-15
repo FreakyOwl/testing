70f1c00434f5c0428af369bb0a5040423bdb7adf
FreakyOwl <69248671+FreakyOwl@users.noreply.github.com>
2020-12-15 15:18:16 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,9 +2,6 @@ import pytest
 import scriptfortests
 
 
-def test_assert():
-    assert scriptfortests.listofurls.name == "123.txt"
-
 def test_exception():
     with pytest.raises(SystemExit):
-        scriptfortests.filefun()
+        scriptfortests.getdata('1234.txt')
