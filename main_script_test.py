a05400c1e1403fc9f2154f7bc08e9eb2a3751357
FreakyOwl <69248671+FreakyOwl@users.noreply.github.com>
2020-12-17 14:53:58 +0300

    Update and rename test1.py to main_script_test.py

--- main_script_test.py
+++ main_script_test.py
@@ -1,14 +1,14 @@
 import pytest
-import scriptfortests
+import main_script
 
 
 def test_exception():
     with pytest.raises(FileNotFoundError):
-        scriptfortests.getdata('1234.txt')
+        main_script.get_data('1234.txt')
         
         
-def test_fileformat():
-    with open('123.txt', 'r') as listofurls:
+def test_urlformat():
+    with open('VK_URLs.txt', 'r') as listofurls:
         for line in listofurls:
             assert line[:15] == 'https://vk.com/'
             assert line[-1] == '\n'
