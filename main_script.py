6929ebbb44cfe6d847a21c58127aaf1a9dfd327b
FreakyOwl <69248671+FreakyOwl@users.noreply.github.com>
2021-01-14 21:05:16 +0300

    Update main_script.py
    Update config.yml
    
    Update config.yml

--- main_script.py
+++ main_script.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3.7
 """
 this is a docstring
@@ -44,50 +43,3 @@ def get_friends():
 
 if __name__ == '__main__':
     get_friends()
-=======
-#!/usr/bin/env python3 
-"""
-this is a docstring
-"""
-
-import urllib.request
-import json
-
-def get_data(filename):
-    """
-    this is a docstring too
-    """
-    listofurls = open(filename, 'r')
-    domain = ''
-    for line in listofurls:                 # получаем информацию о пользователях за один запрос
-        domain = domain+line[15:len(line)-1]+','  # получаем короткое название из url
-    with urllib.request.urlopen("https://api.vk.com/method/users.get?user_ids=" +
-         domain[:len(domain) - 1] +
-   "&v=5.52&access_token=b1b6da65b1b6da65b1b6da6547b1c57304bb1b6b1b6da65eefec3e8bb3149f4bcc040dc")\
-             as userurl:
-        userinfo = json.load(userurl)
-    listofurls.close()
-    return userinfo
-
-def get_friends():
-    """
-    also a docstring
-    """
-    userinfo = get_data('VK_URLs.txt')
-    listsoffriends = open('friendsurls.txt', 'w')
-    for item in userinfo.get('response'):
-        personid = str(item.get('id'))        # получаем ID пользователя
-        with urllib.request.urlopen("https://api.vk.com/method/friends.get?user_id=" + personid +
-   "&v=5.52&access_token=b1b6da65b1b6da65b1b6da6547b1c57304bb1b6b1b6da65eefec3e8bb3149f4bcc040dc")\
-             as friendsurl:
-            friendsinfo = json.load(friendsurl)     # получаем список друзей
-        for friendid in friendsinfo.get('response').get('items'):   # формируем список url-ов друзей
-            friendurl = "https://vk.com/id"+str(friendid)
-            listsoffriends.write(friendurl + '\n')
-        listsoffriends.write('\n')
-    listsoffriends.close()
-
-
-if __name__ == '__main__':
-    get_friends()
->>>>>>> e2369abb22b65c7d9dbd5197f7190de895f241a3
